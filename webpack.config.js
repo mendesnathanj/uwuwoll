72508fa9132beba7d465049ef3ed598cf2848c23
Nathan <mendesnathanj@gmail.com>
2020-03-29 10:48:17 -0700

    Package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var path = require("path");
 var webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: "./frontend/crunchyroll.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "./bundle.js"
   },
   module: {
@@ -22,19 +22,19 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ["file-loader"]
       }
     ]
   },
   devtool: "source-map",
   resolve: {
-    extensions: [".js", ".jsx", "*"],
+    extensions: [".js", ".jsx", "*"]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    })
   ]
 };
