4e753a22f432025ad9d3ebd5ca84081267778347
Nathan <mendesnathanj@gmail.com>
2020-03-29 10:31:55 -0700

    Adjust webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var webpack = require('webpack');
 
 module.exports = {
   entry: "./frontend/crunchyroll.jsx",
@@ -29,5 +30,10 @@ module.exports = {
   devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", "*"],
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
 };
