4792066315bd83b6c7cd1bde15c01fdfbef2298b
Nathan <mendesnathanj@gmail.com>
2020-04-16 11:33:50 -0700

    migrate to browser router

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </Provider>
 );
 
